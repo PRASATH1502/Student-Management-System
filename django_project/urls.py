ca7076ef2db844cff4e60036b071ca2995195eae
unknown <guruprasath254@gmail.com>
2024-10-23 23:35:44 +0530

    Initial commit

---
+++ django_project/urls.py
@@ -0,0 +1,9 @@
+# django_project/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('students.urls')),  # This should match the app name
+]
